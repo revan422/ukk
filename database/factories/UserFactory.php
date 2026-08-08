e3c5ebf6d7f2822b94004ffc67b973891cd9f917
revan422 <mrrvandismktibazma@gmail.com>
2026-08-08 20:09:00 +0700

    b

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,17 +22,16 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'User ' . \Illuminate\Support\Str::random(5),
+            'email' => 'user_' . \Illuminate\Support\Str::random(5) . '@example.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => static::$password ??= \Illuminate\Support\Facades\Hash::make('password'),
+            'remember_token' => \Illuminate\Support\Str::random(10),
         ];
     }
-
     /**
      * Indicate that the model's email address should be unverified.
      */
